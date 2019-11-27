07c85a44022008512fa889a2e5a512a7d4bcd15f
Sterling Watts <SterlingDWatts@gmail.com>
2019-11-27 11:39:43 -0800

    Added Context to avoid prop drilling

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Nav(props) {
-  return (
-    <nav className='Nav'>
-      <Link to={'/'}>
-        Bookmark List
-      </Link>
-      {' '}
-      <Link to={'/add-bookmark'}>
-        Add Bookmark
-      </Link>
-    </nav>
-  );
+    return (
+        <nav className='Nav'>
+            <Link to={'/'}>
+                Bookmark List
+            </Link>
+            {' '}
+            <Link to={'/add-bookmark'}>
+                Add Bookmark
+            </Link>
+        </nav>
+    );
 }
--- src/Nav/Nav.test.js
+++ src/Nav/Nav.test.js
@@ -4,12 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import Nav from './Nav';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <BrowserRouter>
-      <Nav />
-    </BrowserRouter>,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(
+        <BrowserRouter>
+            <Nav />
+        </BrowserRouter>,
+        div
+    );
+    ReactDOM.unmountComponentAtNode(div);
 });
