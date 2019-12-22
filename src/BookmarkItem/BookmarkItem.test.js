1a4b68dc95179deb4a34b99d6afe9f6b05b4dde1
Sterling Watts <SterlingDWatts@gmail.com>
2019-12-22 11:41:19 -0800

    Add custom validation for BookmarkItem and Rating Components

--- src/BookmarkItem/BookmarkItem.test.js
+++ src/BookmarkItem/BookmarkItem.test.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import BookmarkItem from './BookmarkItem';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<BookmarkItem />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
+describe(`BookmarkItem Component`, () => {
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<BookmarkItem title='Google' url='https://www.google.com' />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+})
--- src/BookmarkList/BookmarkList.test.js
+++ src/BookmarkList/BookmarkList.test.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import BookmarkList from './BookmarkList';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<BookmarkList />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
+describe(`BookmarkList Component`, () => {
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<BookmarkList />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+})
