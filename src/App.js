d68e045af6705ba988f145df3416cd90ef866ccc
Sterling Watts <SterlingDWatts@gmail.com>
2020-02-19 15:28:28 -0800

    Add prettier

--- src/App.js
+++ src/App.js
@@ -1,85 +1,88 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import AddBookmark from './AddBookmark/AddBookmark';
-import BookmarkList from './BookmarkList/BookmarkList';
-import BookmarksContext from './BookmarksContext';
-import Nav from './Nav/Nav';
-import config from './config';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import AddBookmark from "./AddBookmark/AddBookmark";
+import BookmarkList from "./BookmarkList/BookmarkList";
+import BookmarksContext from "./BookmarksContext";
+import Nav from "./Nav/Nav";
+import config from "./config";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    bookmarks: [],
+    error: null
+  };
 
-    state = {
-        bookmarks: [],
-        error: null,
-    };
+  setBookmarks = bookmarks => {
+    this.setState({
+      bookmarks,
+      error: null
+    });
+  };
 
-    setBookmarks = bookmarks => {
-        this.setState({
-            bookmarks,
-            error: null,
-        })
-    }
+  addBookmark = bookmark => {
+    this.setState({
+      bookmarks: [...this.state.bookmarks, bookmark]
+    });
+  };
 
-    addBookmark = bookmark => {
-        this.setState({
-            bookmarks: [ ...this.state.bookmarks, bookmark ],
-        })
-    }
+  deleteBookmark = bookmarkId => {
+    const newBookmarks = this.state.bookmarks.filter(
+      bm => bm.id !== bookmarkId
+    );
+    this.setState({
+      bookmarks: newBookmarks
+    });
+  };
 
-    deleteBookmark = bookmarkId => {
-        const newBookmarks = this.state.bookmarks.filter(bm =>
-            bm.id !== bookmarkId
-        )
-        this.setState({
-            bookmarks: newBookmarks
-        })
-    }
+  updateBookmark = bookmarkId => {
+    // TODO Edit bookmark and setState
+  };
 
-    componentDidMount() {
-        fetch(config.API_ENDPOINT, {
-            method: 'GET',
-            headers: {
-                'content-type': 'application/json',
-                'Authorization': `Bearer ${config.API_KEY}`
-            }
-        })
-        .then(res => {
-            if (!res.ok) {
-                throw new Error(res.status)
-            }
-            return res.json()
-        })
-        .then(this.setBookmarks)
-        .catch(error => this.setState({ error }))
-    }
-
-    render() {
-        const contextValue = {
-            bookmarks: this.state.bookmarks,
-            addBookmark: this.addBookmark,
-            deleteBookmark: this.deleteBookmark,
+  componentDidMount() {
+    fetch(config.API_ENDPOINT, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${config.API_KEY}`
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(res.status);
         }
-        return (
-            <main className='App'>
-                <h1>Bookmarks!</h1>
-                <BookmarksContext.Provider value={ contextValue }>
-                    <Nav />
-                    <div className='content' aria-live='polite'>
+        return res.json();
+      })
+      .then(this.setBookmarks)
+      .catch(error => this.setState({ error }));
+  }
+
+  render() {
+    const contextValue = {
+      bookmarks: this.state.bookmarks,
+      addBookmark: this.addBookmark,
+      deleteBookmark: this.deleteBookmark,
+      updateBookmark: this.updateBookmark
+    };
+    return (
+      <main className="App">
+        <h1>Bookmarks!</h1>
+        <BookmarksContext.Provider value={contextValue}>
+          <Nav />
+          <div className="content" aria-live="polite">
+            <Route path="/add-bookmark" component={AddBookmark} />
+            <Route exact path="/" component={BookmarkList} />
+            {/* // TODO add EditBookmark component
                         <Route
-                            path='/add-bookmark'
-                            component={ AddBookmark }
+                            path="/edit/:bookmarkId"
+                            component={ EditBookmark }
                         />
-                        <Route
-                            exact
-                            path='/'
-                            component={ BookmarkList }
-                        />
-                    </div>
-                </BookmarksContext.Provider>
-            </main>
-        );
-    }
+                        */}
+          </div>
+        </BookmarksContext.Provider>
+      </main>
+    );
+  }
 }
 
 export default App;
